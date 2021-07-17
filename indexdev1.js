fd44b41256b4115d27fbc4c59ac3759f0089a7ec
Shivang <shivangagrahari2000@gmail.com>
2021-07-18 02:36:29 +0530

    Add files via upload

---
+++ indexdev1.js
@@ -0,0 +1,67 @@
+console.log("the scriprt works")
+
+var categ = document.getElementById('categories');
+
+
+var cat = document.getElementById('Category')
+cat.style.display = 'none';
+categ.addEventListener('mouseenter',function() {
+
+cat.style.display = 'block';
+})
+categ.addEventListener('mouseleave', function() {
+
+  cat.style.display = 'none';
+})
+
+
+var imge = document.getElementById('shivang');
+// imge.style.width = "75px";
+// imge.style.height ="75px";
+var deve = document.getElementById('develper');
+//
+
+var detal = document.getElementById('det')
+
+detal.style.display = "none"
+
+deve.onmouseenter = function() {
+  deve.style.width = "150px";
+  deve.style.height = "300px";
+  deve.style.opacity = "1.0";
+
+  imge.style.width = "150px";
+  imge.style.height = "150px";
+  imge.style.opacity = "1.0";
+
+  detal.style.display = "block";
+
+}
+
+deve.onmouseleave = function() {
+  deve.style.width = "80px";
+  deve.style.height = "80px";
+  deve.style.opacity = "0.6"
+  imge.style.width = "80px";
+  imge.style.height = "80px";
+  imge.style.opacity = "0.6";
+  detal.style.display = "none";
+
+}
+
+imge.onclick = function() {
+
+var redirect = window.confirm('You want to connect on LinkedIn')
+
+
+  if(redirect == true){
+    window.location.assign('https://in.linkedin.com/in/shivang-agrahari')
+  }
+   else{
+     alert('thank you for visiting the site')
+   }}
+
+// //
+// deve.addEventListener('mouseover', function() {
+//
+// })

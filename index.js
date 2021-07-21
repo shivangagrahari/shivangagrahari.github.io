5a7849b80cb732ce7e0b3a279d656d7c9f6eb287
Shivang <shivangagrahari2000@gmail.com>
2021-07-21 14:26:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var wk = document.getElementsByClassName('wrk');
+var sc = document.getElementsByClassName('work')
+
+console.log('JS loaded');
+
+// wk.addEventListener('mouseenter', function() {
+//   console.log('mouseenter');
+// sc.style.transitionDuartion = '12s';
+//   sc.style.visibility = 'visible';
+//
+//
+// })
+// wk.addEventListener('mouseleave', function() {
+//   console.log('mouseenter');
+//   sc.style.visibility= 'hidden';
+//
+// })

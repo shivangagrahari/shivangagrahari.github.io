c57745627df34954ea6b6bdf1c8e39ffeaeddc9b
Shivang <shivangagrahari2000@gmail.com>
2023-03-12 11:46:36 +0530

    adding eye tracker
    adding eye tracker

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-var maillink = document.getElementById('mail');
-var jsmail = document.getElementById('mailtoid');
 
 
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
+      // Set initial eye position
+      let eyeX = canvas.width / 2;
+      let eyeY = canvas.height / 2;
 
+      // Set up mouse move event listener
+      canvas.addEventListener("mousemove", function(event) {
+        // Update eye position based on mouse position
+        eyeX = event.clientX;
+        eyeY = event.clientY;
+      });
 
- var mover = document.getElementById('imain');
+      // Draw the eye and update its position
+      function drawEye() {
+        // Clear canvas
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-mover.addEventListener("mouseover", function(){
-  document.getElementById('sclick').click();
+        // Draw eye background
+        context.beginPath();
+        context.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2);
+        context.fillStyle = "#fff";
+        context.fill();
+        context.stroke();
 
+        // Draw iris
+        const dx = eyeX - canvas.width / 2;
+        const dy = eyeY - canvas.height / 2;
+        const angle = Math.atan2(dy, dx);
+        const irisX = canvas.width / 2 + Math.cos(angle) * 25;
+        const irisY = canvas.height / 2 + Math.sin(angle) * 25;
+        context.beginPath();
+        context.arc(irisX, irisY, 10, 0, Math.PI * 2);
+        context.fillStyle = "#000";
+        context.fill();
 
-  //document.getElementById('sclick').onclick = function() { alert("click")}
-// document.getElementById('sclick').click();
+        // Draw pupil
+        context.beginPath();
+        context.arc(irisX, irisY, 5, 0, Math.PI * 2);
+        context.fillStyle = "#fff";
+        context.fill();
 
-})
+        // Update eye position
+        requestAnimationFrame(drawEye);
+      }
+
+      // Start animation
+      drawEye();

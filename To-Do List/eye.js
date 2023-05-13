0b6322f7557c8954b7b55fdfa255cf407470c4e1
Shivang <shivangagrahari2000@gmail.com>
2023-05-13 23:40:05 +0530

    eye gazer

--- To-Do List/eye.js
+++ To-Do List/eye.js
@@ -1,22 +1,16 @@
-const eye = document.querySelector('.eye');
-const iris = document.querySelector('.iris');
-const pupil = document.querySelector('.pupil');
-
-document.addEventListener('mousemove', (e) => {
-  const x = -e.clientX;
-  const y = -e.clientY;
-  const eyeRect = eye.getBoundingClientRect();
-  const eyeCenterX = eyeRect.left + eyeRect.width / 2;
-  const eyeCenterY = eyeRect.top + eyeRect.height / 2;
-  const dx = x - eyeCenterX;
-  const dy = y - eyeCenterY;
-  const distance = Math.sqrt(dx*dx + dy*dy);
-  const maxDistance = Math.min(eyeRect.width / 2, eyeRect.height / 2) - pupil.clientWidth / 2;
-  const angle = Math.atan2(dy, dx);
-  const limitX = Math.cos(angle) * maxDistance;
-  const limitY = Math.sin(angle) * maxDistance;
-  const pupilX = Math.max(-limitX, Math.min(limitX, dx));
-  const pupilY = Math.max(-limitY, Math.min(-limitY, dy));
-  pupil.style.transform = `translate(${pupilX}px, ${pupilY}px)`;
-  iris.style.transform = `translate(${pupilX/10}px, ${pupilY/10}px)`;
+document.addEventListener("mousemove", function(event) {
+  var eye = document.querySelector(".eye");
+  var iris = document.querySelector(".iris");
+  var pupil = document.querySelector(".pupil");
+  var eyeRect = eye.getBoundingClientRect();
+  var centerX = eyeRect.left + eyeRect.width / 2;
+  var centerY = eyeRect.top + eyeRect.height / 2;
+  var angle = Math.atan2(event.pageY - centerY, event.pageX - centerX);
+  var radius = eyeRect.width / 2 - iris.offsetWidth / 2;
+  var posX = centerX + radius * Math.cos(angle);
+  var posY = centerY + radius * Math.sin(angle);
+  iris.style.left = posX + "px";
+  iris.style.top = posY + "px";
+  pupil.style.left = posX + "px";
+  pupil.style.top = posY + "px";
 });
